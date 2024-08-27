debc8a277ec04075c6897f17fb73b88631deb624
iamaquila <zeynepaysuaydogan@gmail.com>
2024-08-27 17:53:06 +0300

    homepage almost done

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -4,8 +4,7 @@ from .views import task_add
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/homepage', views.homepage, name='homepage'),
+    path('', views.homepage, name='homepage'),
     path('add/', task_add, name='task_add.html'),
     path('add_todo/', views.add_todo, name='add_todo'),
 ]
